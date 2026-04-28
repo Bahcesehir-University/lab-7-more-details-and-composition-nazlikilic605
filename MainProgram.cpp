1dd010f69c9f7d5ce11e4e18aa5f5f0adb2b8e01
nazlikilic605 <nazlinur.kilic@bahcesehir.edu.tr>
2026-04-28 14:47:31 +0300

    Update MainProgram.cpp

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -78,7 +78,7 @@ public:
 
 // TODO 12: implement isSameSize
 bool isSameSize(const Rectangle& r1, const Rectangle& r2) {
-    return (r1.getArea() == r2.getArea()) && (r1.getHeight() == r2.getHeight());
+    return (r1.getArea() == r2.getArea();
      // stub
      
 }
